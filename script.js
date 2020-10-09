0331a5ee8a49951ac6c3faf7bc9c8e6a09e1541e
abc <abc@example.com>
2020-10-09 15:58:02 -0700

    loading fixed

--- script.js
+++ script.js
@@ -47,18 +47,18 @@ async function getQuote() {
     getQuote();
   }
 }
+
 // Tweet Quote
 function tweetQuote() {
-    const quote = quoteText.innerText;
-    const author = authorText.innerText;
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
-    window.open(twitterUrl, '_blank');
-  }
-  
-  // Event Listeners
-  newQuoteBtn.addEventListener('click', getQuote);
-  twitterBtn.addEventListener('click', tweetQuote);
-  
-  // On Load
-  getQuote();
-  
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On Load
+getQuote();
